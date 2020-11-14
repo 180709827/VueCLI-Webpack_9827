5a81f54958e8922cfb5359c61f35bbb21198a735
ACER <aldovarian109@gmail.com>
2020-11-14 20:53:04 +0700

    Tugas Vue CLI

--- src/router.js
+++ src/router.js
@@ -35,6 +35,12 @@ const router = new VueRouter({
                 name: "Unguided",
                 component: importComponent('TodoList/ListUGD'),
             },
+             // To do list TGS
+             {
+                path: "/tgs",
+                name: "Tugas",
+                component: importComponent('TodoList/ListTugas'),
+            },
            ]
        },
    ]
